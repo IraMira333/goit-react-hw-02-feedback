987f1e77abac54fe7e10e0775cc099d6a55b683d
IraMira333 <irinat333@gmail.com>
2023-06-26 00:56:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+
+export class App extends React.Component {
+  state = {
+    good: 3,
+    neutral: 2,
+    bad: 2,
+  };
+  handleIncrement = e => {
+    if (e.currentTarget.name === 'good') {
+      this.setState(prevState => ({
+        good: prevState.good + 1,
+      }));
+    }
+    if (e.currentTarget.name === 'neutral') {
+      this.setState(prevState => ({
+        neutral: prevState.neutral + 1,
+      }));
+    }
+    if (e.currentTarget.name === 'bad') {
+      this.setState(prevState => ({
+        bad: prevState.bad + 1,
+      }));
+    }
+  };
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(
+      (this.state.good * 100) /
+        (this.state.good + this.state.neutral + this.state.bad)
+    );
+  };
+  render() {
+    return (
+      <div
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <FeedbackOptions onLeaveFeedback={this.handleIncrement} />
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
+      </div>
+    );
+  }
+}
