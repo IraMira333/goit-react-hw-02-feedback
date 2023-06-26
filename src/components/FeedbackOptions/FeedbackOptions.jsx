a105b4a331201cf4c64fb9c3b97bc5de3241324b
IraMira333 <irinat333@gmail.com>
2023-06-26 15:02:13 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './FeedbackOptions.css';
+import css from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <div>
+  <div className={css.feedbacOptionskBox}>
     {options.map(option => (
       <button
+        className={css.feedbackOptionsBtn}
         type="button"
         key={option}
         name={option}
